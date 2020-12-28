8c9a29163abf263bd0305bc779a956c664c9437a
JackSu-cc <929013002@qq.com>
2020-12-28 21:32:39 +0800

    111

--- CoreApl/Program.cs
+++ CoreApl/Program.cs
@@ -15,7 +15,7 @@ namespace CoreApl
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
