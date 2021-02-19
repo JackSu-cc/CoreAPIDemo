26e4bb27b48700a10c3682d96196b3f140e9060e
cc <929013002@qq.com>
2021-02-19 10:58:02 +0800

    111

---
+++ Service/ViewModel/Log/ErrorLogRequestVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+/******************************************************
+ * 公司名称：       智驾出行
+ * 创 建 人：       kongdf
+ * 创建时间：       2021/2/19 10:10:56
+ * 说    明：
+*******************************************************/
+namespace Application.ViewModel.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ErrorLogRequestVM
+    {
+                       
+        
+    }
+}
